e38994578c7a9efddf0cb377289fc363f15f2eb5
Manuel García <Lobos@MacBook-Adry-y-Manu.local>
2019-12-10 13:46:25 -0600

    feat: Unit testing when building phone number from array

---
+++ test/function-test/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const assert = require('chai').assert;
+const createPhoneNumber = require('../../src/function-test/index');
+
+suite('All cases', () => {
+    test('Success case', () => {
+        assert.equal(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+    });
+    test('Success case', () => {
+        assert.equal(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+    });
+    test('Success case', () => {
+        assert.equal(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+    });
+    test('Fail case', () => {
+        assert.equal(createPhoneNumber(['a', 2, 3, 4, 5, 6, 7, 8, 9, 0]), "All elements need to be numbers");
+    });
+});
